5a8307b9ecc4fda9d9b796daa78f49acdaf6d22d
Mola <huylaqn1008@gmail.com>
2023-02-26 17:33:58 +0700

    add backend

---
+++ webflowwer/frontend/src/data.js
@@ -0,0 +1,27 @@
+const data = {
+    products: [{
+            name: 'Rose Red',
+            category: 'Rose',
+            image: '/images/rosered.jpg',
+            prices: 20,
+            stock: 50,
+            description: 'Beautiful flower for love',
+        },
+        {
+            name: 'Rose Red',
+            category: 'Rose',
+            image: '/images/rosered2.jpg',
+            prices: 20,
+            stock: 50,
+            description: 'Beautiful flower for love',
+        },
+        {
+            name: 'Rose Red',
+            category: 'Rose',
+            image: '/images/rosered3.jpg',
+            prices: 20,
+            stock: 50,
+            description: 'Beautiful flower for love',
+        },
+    ]
+}
