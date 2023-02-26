45fa959669356784d7eabfceda48650c08bc6500
Mola <huylaqn1008@gmail.com>
2023-02-26 17:33:23 +0700

    add backend

--- webflowwer/frontend/src/App.js
+++ webflowwer/frontend/src/App.js
@@ -2,23 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return ( <
-        div >
-        <
-        header >
-        <
-        a href = "/" > Web Flowers < /a> <
-        /header> <
-        main >
-        <
-        h1 > Feature Products < /h1> <
-        div className = "products" >
 
-        <
-        /div> <
-        /main> <
-        /div>
-    );
 }
 
 export default App;
