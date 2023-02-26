5675dbe5653f1311f8de3fff4c09213f77a70488
Mola <huylaqn1008@gmail.com>
2023-02-26 15:44:56 +0700

    web-flower

---
+++ webflowwer/frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "App" >
+        <
+        header >
+        <
+        a hred = "/" > Wed flower < /a> <
+        /header> <
+        main > list product < /main> <
+        /div>
+    );
+}
+
+export default App;
