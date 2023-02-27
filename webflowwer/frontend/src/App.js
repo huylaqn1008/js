953e532d385b9381d8846580007e46eafe61ab00
Mola <huylaqn1008@gmail.com>
2023-02-27 23:25:39 +0700

    commit again

--- webflowwer/frontend/src/App.js
+++ webflowwer/frontend/src/App.js
@@ -1,8 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-
+  return (
+    <div>
+      <Header/>
+        <main className='pt-16 bg-slate-300 min-h-[calc(100vh)]'>
+          <Outlet/>
+        </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ webflowwer/frontend/src/components/Header.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../assets/logo.png';
+import {HiOutlineUserCircle} from 'react-icons/hi';
+import {FaShoppingCart} from 'react-icons/fa';
+
+const Header = () => {
+  const[showMenu,setShowMenu] = useState(false)
+  const handleShowMenu = () => {
+    setShowMenu(preve => !preve)
+  }
+  return (  
+    <header className='fixed shadow-md w-full h-36 px-2 md:px-4 z-50 bg-white' style={{ borderBottom: '2px solid #ccc' }}>
+      <div className='flex items-center justify-between'>
+        <Link to={''}>
+          <div className="">
+            <img src={logo} className="h-full"/>
+          </div>
+        </Link>
+        <div className='flex items-center gap-4 md:gap-7'>
+          <Link to={'/'} className='mx-4 text-gray-600 hover:text-gray-800 my-2 md:my-0' style={{ fontSize: '1.6rem' }}>
+            Home
+          </Link>
+          <Link to={'/menu'} className='mx-4 text-gray-600 hover:text-gray-800 my-2 md:my-0' style={{ fontSize: '1.6rem' }}>
+            Menu
+          </Link>
+          <Link to={'/about'} className='mx-4 text-gray-600 hover:text-gray-800 my-2 md:my-0' style={{ fontSize: '1.6rem' }}>
+            About
+          </Link>
+          <Link to={'/contact'} className='mx-4 text-gray-600 hover:text-gray-800 my-2 md:my-0' style={{ fontSize: '1.6rem' }}>
+            Contact
+          </Link>
+          <div className='text-2x1 text-slate-600 relative'>
+            <Link to={'/cart'} className='ml-10 mx-4 text-gray-600 hover:text-gray-800'>
+              <FaShoppingCart size={30} />
+              < div className='absolute left-5 top-5 text-white bg-red-500 h-5 w-5 rounded-full m-0 text-sm text-center'>0</div>
+            </Link>
+          </div>
+          <div className='text-slate-600' onClick={handleShowMenu}>
+            <div className='text-3x1 cursor-pointer'>
+              <div className='mx-4 text-gray-600 hover:text-gray-800'>
+                <HiOutlineUserCircle size={38} />
+              </div>
+            </div>
+            {showMenu && (
+              <div className='absolute bottom-1 right-2 bg-white py-1 px-2 shadow drop-shadow-md'>
+                <Link to={'/newproduct'} className='whitespace-nowrap cursor-pointer'>New product</Link>
+                <Link to={'/login'} className='whitespace-nowrap cursor-pointer'>Login</Link>
+              </div>
+            )}
+          </div> 
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
